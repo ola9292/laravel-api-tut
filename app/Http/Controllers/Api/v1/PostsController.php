7a463d5df893d04c3e02f4552361cebe29cf4b5a
admin <admin@admins-MBP.lan>
2026-02-15 16:33:07 +0000

    second commit

---
+++ app/Http/Controllers/Api/v1/PostsController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::with('user')->paginate();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => ['required', 'string', 'min:2'],
+            'body' => ['required', 'string', 'min:2'],
+        ]);
+
+        $data['user_id'] = 1;
+
+        $post = Post::create($data);
+
+        return response()->json([
+            'data' => $post,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::findOrFail($id);
+
+        return response()->json([
+            'data' => $post->load('user'),
+        ], 200);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->validate([
+            'title' => ['required'],
+            'body' => ['required'],
+        ]);
+
+        $post = Post::findOrFail($id);
+
+        $post->update($data);
+
+        return response()->json([
+            'data' => $post,
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return response()->noContent();
+    }
+}
